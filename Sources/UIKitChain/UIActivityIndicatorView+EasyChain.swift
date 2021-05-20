826bdc07c65ec9edcf56bccc6d459e28ddfc939e
罗树新 <luoshuxin@renrenche.com>
2021-05-20 13:31:28 +0800

    MOD:  增加 UIActivityIndicatorView + EasyChain

---
+++ Sources/UIKitChain/UIActivityIndicatorView+EasyChain.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by 罗树新 on 2021/5/20.
+//
+
+import UIKit
+import EasyChainCore
+
+public extension EasyChain where Base: UIActivityIndicatorView {
+    
+    @discardableResult func style(_ style: UIActivityIndicatorView.Style) -> Self {
+        return make { $0.style = style }
+    }
+    
+    @discardableResult func isHidesWhenStopped(_ isHidesWhenStopped: Bool) -> Self {
+        return make { $0.hidesWhenStopped = isHidesWhenStopped }
+    }
+
+    @available(iOS 5.0, *)
+    @discardableResult func color(_ color: UIColor) -> Self {
+        return make { $0.color = color }
+    }
+
+}
